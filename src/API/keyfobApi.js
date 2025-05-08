e24e1eef5d529d5561d682962fbafceeeaa1f448
DarinelGuillen <darinel.escobar@outlook.com>
2025-05-08 09:13:50 -0600

    ♻️ refactor(API): update API URL configuration
    - replace hardcoded API URLs with environment variable for flexibility
    - ensure trailing slash is appended to API URL if not present

--- src/API/keyfobApi.js
+++ src/API/keyfobApi.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_URL = "http://localhost/AmiFitCompleto/";
+const API_URL = process.env.REACT_APP_API_URL?.endsWith('/')
+  ? process.env.REACT_APP_API_URL
+  : process.env.REACT_APP_API_URL + '/';
 
 
 export const validarClave = clave => {
